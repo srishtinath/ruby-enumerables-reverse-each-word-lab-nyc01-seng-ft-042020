eba0902327566b33f41057c4f646fd0c6d097120
Srishti Nath <srishti.nath@gmail.com>
2020-04-13 16:16:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
-  phrase_array = string.split
   reverse_phrase = []
+  phrase_array = string.split
   phrase_array.each do |word|
     reverse_phrase << word.reverse
   end

8c2952c61b060f74dbd204bf45c61bc3bdfcf66f
Srishti Nath <srishti.nath@gmail.com>
2020-04-13 16:15:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
 def reverse_each_word(string)
   phrase_array = string.split
-  reverse = phrase_array.each { |word| word.reverse}
+  reverse = []
+  phrase_array.each do |word|
+    reverse << word.reverse
+  end
   reverse.join(" ")
 end
